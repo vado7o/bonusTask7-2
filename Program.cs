5ea69ecca6a3b7b8137a6b05756f415c526ead03
Vadim Pudov <pvb@rambler.ru>
2022-10-01 15:14:16 +0700

    change again

--- Program.cs
+++ Program.cs
@@ -6,48 +6,68 @@
 Console.Clear();
 Console.WriteLine("Задача с очень длинным описанием (доп задача со звёздочкой)");
 int size = 0;
+int size2 = 0;
 Random rnd = new Random();
 List<int> array1 = new List<int>();
 List<int> array2 = new List<int>();
 double sum = 0.0;
+double average1 = 0.0;
+double average2 = 0.0;
 
-while(true) {
-Console.WriteLine("\nНапишите - из скольки элементов должны состоять одномерные массивы? : ");
-if(int.TryParse(Console.ReadLine(), out size)) {
-    if(size > 1) {
-        break;
-    } else Console.WriteLine("Некорректно указано количество элементов массивов!\n");
-} else Console.WriteLine("Некорректно указано количество элементов массивов!\n");
-}
+SelectSize(1);
+SelectSize(2);
 
-for(int index = 0; index < size; index++) {
-    array1.Add(rnd.Next(1, 10));
-    array2.Add(rnd.Next(1, 10));
-}
+FillArray(size, array1);
+FillArray(size2, array2);
 
-Console.WriteLine("\nСреднее арифметическое первого массива ");
-CalcAndPrint(array1);
-Console.WriteLine(" = " + sum / Convert.ToDouble(size));
-double average1 = sum / Convert.ToDouble(size);
-Console.WriteLine("\nСреднее арифметическое второго массива ");
-CalcAndPrint(array2);
-Console.WriteLine(" = " + sum / Convert.ToDouble(size));
-double average2 = sum / Convert.ToDouble(size);
-
+CalcAverage(1, array1, size);
+CalcAverage(2, array2, size2);
 
 int result = Math.Abs((int) Math.Round(average1 - average2));
 Console.WriteLine("\nРазница между средними арифметическими заданных массивов = " + result);
 Console.WriteLine("\nЧисло " + result + " в двоичной системе исчисления = " + Convert.ToString(result, 2));
 
 
-void CalcAndPrint(List<int> array) {
+void CalcAndPrint(List<int> array, int size) {
     sum = 0.0;
     for(int index = 0; index < size; index++) {
         sum += Convert.ToDouble(array[index]);
         if(index == 0) {
-            Console.Write("[" + array[index] + ", ");
+            Console.Write("\n[" + array[index] + ", ");
         } else if(index == size - 1) {
             Console.Write(array[size - 1] + "]");
         } else Console.Write(array[index] + ", ");
     }
+}
+
+void SelectSize(int variant) {
+while(true) {
+    Console.Write("\nНапишите - из скольки элементов должен состоять следующий массив? : ");
+    if(int.TryParse(Console.ReadLine(), out int number)) {
+        if(number > 1) {
+            if(variant == 1) {
+                size = number;
+            } else { size2 = number; }    
+        break;
+        } else Console.WriteLine("Некорректно указано количество элементов первого массива!\n");
+    } else Console.WriteLine("Некорректно указано количество элементов первого массива!\n");
+}
+}
+
+void FillArray(int size, List<int> array) {
+for(int index = 0; index < size; index++) {
+    array.Add(rnd.Next(1, 10));
+}
+}
+
+void CalcAverage(int variant, List<int> array, int size) {
+CalcAndPrint(array, size);
+if(variant == 1) {
+    Console.WriteLine("\nСреднее арифметическое первого массива ");
+    average1 = sum / Convert.ToDouble(size);
+} else {
+    Console.WriteLine("\nСреднее арифметическое второго массива ");
+    average2 = sum / Convert.ToDouble(size);
+}
+Console.WriteLine(" = " + sum / Convert.ToDouble(size));
 }
